def1f437527ce64fd931c85ad51744dc0c6fdcd7
Germano Keller <germano@email.com>
2025-07-23 08:16:42 -0300

    Adicionando classes geradas por StarUML

---
+++ code/enemy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Class6 import Class6
+from Entity import Entity
+
+
+class Enemy(Class6, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
