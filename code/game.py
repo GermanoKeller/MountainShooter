08be97659551b9b5df7310a1df7f0b24b2ae248d
Germano Keller <germano@email.com>
2025-07-29 14:03:13 -0300

    Criando o menu: imagem de fundo, musica e fontes

--- code/game.py
+++ code/game.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame as pg
+import pygame 
 
-from code.menu import Menu
+from code.Const import WIN_HEIGHT, WIN_WIDTH
+from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
-        pg.init()
-        self.window = pg.display.set_mode(size=(600, 480))
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
-        menu = Menu(self.window)
-        menu.run()
-        pass
-
-        # while True:
-        # # check for all events
-        #     for event in pg.event.get():
-        #         if event.type == pg.QUIT:
-        #             pg.quit() # Close window
-        #             quit() # end pygame
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
